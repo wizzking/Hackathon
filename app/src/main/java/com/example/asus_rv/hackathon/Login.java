2f48ab7eaf03c1f0a121b249b07cc268a4ad9c5d
WizzKing <cotafiesta@gmail.com>
2018-04-16 16:00:06 -0600

    bavbar

--- app/src/main/java/com/example/asus_rv/hackathon/Login.java
+++ app/src/main/java/com/example/asus_rv/hackathon/Login.java
@@ -1,7 +1,10 @@
 package com.example.asus_rv.hackathon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +12,33 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+        Button Home_ = (Button) findViewById(R.id.Home);
+        Button Register_ = (Button) findViewById(R.id.Registre);
+
+
+
+        Home_.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent OpenMyHome=new Intent(Login.this,Home.class);
+
+                startActivity(OpenMyHome);
+            }
+        });
+
+
+        Register_.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent OpenMyRegister=new Intent(Login.this,Register.class);
+
+                startActivity(OpenMyRegister);
+            }
+        });
+
+
     }
 }
