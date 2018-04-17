cb53469d22e276b841ab7cbb4c10ee635c8ea1d5
WizzKing <cotafiesta@gmail.com>
2018-04-17 12:44:14 -0600

    registro

--- app/src/main/java/com/example/asus_rv/hackathon/Register.java
+++ app/src/main/java/com/example/asus_rv/hackathon/Register.java
@@ -5,8 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.github.nkzawa.socketio.client.IO;
@@ -18,25 +21,50 @@ import java.net.URISyntaxException;
 
 public class Register extends AppCompatActivity {
     private Socket mSocket;
+    private String tipo;
     public void conectar ()
     {
-        EditText DataUser2 = (EditText) findViewById(R.id.User);
-        String FinalUser2 = DataUser2.getText().toString();
+        EditText DataName = (EditText) findViewById(R.id.RNombre);
+        String FinalDataName = DataName.getText().toString();
 
-        EditText DataPass2 = (EditText) findViewById(R.id.Pass);
-        String FinalPass2 = DataPass2.getText().toString();
+        EditText DataLastName1 = (EditText) findViewById(R.id.RLastName1);
+        String FinalDataLastName1 = DataLastName1.getText().toString();
 
-        if(TextUtils.isEmpty(FinalUser2) || TextUtils.isEmpty(FinalPass2)){
+        EditText DataLastName2 = (EditText) findViewById(R.id.RLastName2);
+        String FinalDataLastName2 = DataLastName2.getText().toString();
+
+        EditText DataPass = (EditText) findViewById(R.id.RPass);
+        String FinalDataPass = DataPass.getText().toString();
+
+        EditText DataRPass = (EditText) findViewById(R.id.RRPass);
+        String FinalDataRPass = DataRPass.getText().toString();
+
+        EditText DataCorreo = (EditText) findViewById(R.id.RCorreo);
+        String FinalDataCorreo = DataCorreo.getText().toString();
+
+        if(TextUtils.isEmpty(FinalDataName) || TextUtils.isEmpty(FinalDataLastName1)
+                || TextUtils.isEmpty(FinalDataLastName2) || TextUtils.isEmpty(FinalDataPass)
+                || TextUtils.isEmpty(FinalDataRPass) || TextUtils.isEmpty(FinalDataCorreo)){
             Toast.makeText(Register.this, "Favor de Completar los campos.", Toast.LENGTH_SHORT).show();
         }
         else{
-            try {
-                mSocket = IO.socket("http://192.168.8.27:90");
-            } catch (URISyntaxException e) {}
+            if (FinalDataPass.equals(FinalDataRPass))
+            {
+                try {
+                    mSocket = IO.socket("http://192.168.8.27:90");
+                } catch (URISyntaxException e) {}
 
-            mSocket.on("getResponse",getResponseRegister);
-            mSocket.on("sendDatosUserRegister",sendDatosUserRegister);
-            mSocket.connect();
+                mSocket.on("getResponse",getResponseRegister);
+                mSocket.on("sendDatosUserRegister",sendDatosUserRegister);
+                mSocket.connect();
+
+            }
+            else
+            {
+                Toast.makeText(Register.this, "Favor de ingresar contraseñas iguales.", Toast.LENGTH_SHORT).show();
+
+            }
+
         }
     }
 
@@ -46,17 +74,31 @@ public class Register extends AppCompatActivity {
                 @Override
                 public void run()
                 {
-                    EditText DataUser = (EditText) findViewById(R.id.User);
-                    String FinalUser = DataUser.getText().toString();
+                    EditText DataName = (EditText) findViewById(R.id.RNombre);
+                    String FinalDataName = DataName.getText().toString();
 
-                    EditText DataPass = (EditText) findViewById(R.id.Pass);
-                    String FinalPass = DataPass.getText().toString();
+                    EditText DataLastName1 = (EditText) findViewById(R.id.RLastName1);
+                    String FinalDataLastName1 = DataLastName1.getText().toString();
+
+                    EditText DataLastName2 = (EditText) findViewById(R.id.RLastName2);
+                    String FinalDataLastName2 = DataLastName2.getText().toString();
+
+                    EditText DataPass = (EditText) findViewById(R.id.RPass);
+                    String FinalDataPass = DataPass.getText().toString();
+
+
+                    EditText DataCorreo = (EditText) findViewById(R.id.RCorreo);
+                    String FinalDataCorreo = DataCorreo.getText().toString();
 
                     SocketData cont= new SocketData();
-                    cont.User        = FinalUser;
-                    cont.Password    = FinalPass;
-                    cont.Status       = "1";
-                    cont.Type   = "Person";
+                    cont.RNombre        = FinalDataName;
+                    cont.RLastName1    = FinalDataLastName1;
+                    cont.RLastName2       = FinalDataLastName2;
+                    cont.RPassword   = FinalDataPass;
+                    cont.RLatitud   = "123654";
+                    cont.RCombo   = tipo;
+                    cont.RCorreo = FinalDataCorreo;
+                    cont.RLongitud   = "-78545";
                     Gson gson=new Gson();
                     mSocket.emit("sendDatosUserRegister",gson.toJson(cont));
                     //String g = args[0].toString();
@@ -74,23 +116,9 @@ public class Register extends AppCompatActivity {
                 {
                     Gson gson=new Gson();
                     SocketData msg=gson.fromJson(args[0].toString(),SocketData.class);
-                    //Toast.makeText(Login.this, msg.Respuesta, Toast.LENGTH_SHORT).show();
-                    if (msg.Respuesta.equals("Persona"))
-                    {
-                        Toast.makeText(Register.this, "welcome Persona", Toast.LENGTH_SHORT).show();
-                        Intent OpenMyHome=new Intent(Register.this,Home.class);
-                        startActivity(OpenMyHome);
-                    }
-                    else if (msg.Respuesta.equals("Fijo"))
-                    {
-                        Toast.makeText(Register.this, "Welcome Fijo", Toast.LENGTH_SHORT).show();
-                        Intent OpenMyHome2=new Intent(Register.this,Home.class);
-                        startActivity(OpenMyHome2);
-                    }
-                    else
-                    {
-                        Toast.makeText(Register.this, "No se encontro la cuentaad", Toast.LENGTH_SHORT).show();
-                    }
+                    Toast.makeText(Register.this, msg.Respuesta, Toast.LENGTH_SHORT).show();
+
+                    mSocket.disconnect();
                 }
             });
         }
@@ -101,6 +129,27 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
 
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        String[] valores = {"Person","Fijo"};
+        spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, valores));
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id)
+            {
+                tipo = (String) adapterView.getItemAtPosition(position);
+               // Toast.makeText(adapterView.getContext(), tipo, Toast.LENGTH_SHORT).show();
+
+            }
+
+
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+                // vacio
+
+            }
+        });
+
         Button Ok_ = (Button) findViewById(R.id.Ok);
         Button Exit_ = (Button) findViewById(R.id.Exit);
 
@@ -108,7 +157,7 @@ public class Register extends AppCompatActivity {
 
         Ok_.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
+                conectar();
             }
         });
 
--- app/src/main/java/com/example/asus_rv/hackathon/SocketData.java
+++ app/src/main/java/com/example/asus_rv/hackathon/SocketData.java
@@ -3,5 +3,7 @@ package com.example.asus_rv.hackathon;
 public class SocketData {
     //conexión datos Login
     public String User,Password,Status,Type,idSocket,msg,Respuesta;
+    //Registro
+    public String RNombre,RLastName1,RLastName2,RPassword,RLongitud,RCombo,RLatitud,RCorreo;
 
 }
